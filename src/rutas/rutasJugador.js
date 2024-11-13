7d93013faf0862ecba6223ea16140abc1408fbd6
afcastaneda <afcastaneda1992@live.com>
2024-11-12 23:00:50 -0500

    Avance1
    Avance Video 1

---
+++ src/rutas/rutasJugador.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const enrutador = express.Router();
+const jugadorControlador = require('../controladores/jugadorControlador');
+
+enrutador.post('/registrar', jugadorControlador.registrarJugador);
+enrutador.get('/obtener', jugadorControlador.obtenerJugadores);
+
+module.exports = enrutador;
